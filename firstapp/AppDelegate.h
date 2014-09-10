d43e305ae2422ad476ec38d5f26faca5113f3601
Armando <armando@Armandos-Mac-mini.local>
2014-09-10 11:48:55 +0530

    Initial Commit

---
+++ firstapp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  firstapp
+//
+//  Created by Armando on 10/09/14.
+//  Copyright (c) 2014 Armando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ firstapp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  firstapp
+//
+//  Created by Armando on 10/09/14.
+//  Copyright (c) 2014 Armando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
